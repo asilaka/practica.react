0a47063bb24601f72fa99a341937956b2f65df30
asilaka <abduzalilovasilbek27@gmail.com>
2024-10-21 17:06:22 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./App.css"
+import Card from './components/Card/Card'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Wrapper from './components/Wrapper/Wrapper'
+const App = () => {
+  return (
+   <>
+   <Card   />
+   <About />
+   <Footer/>
+   <Header/>
+   <Hero/>
+   <Wrapper/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Wrapper.css'
+const Wrapper = () => {
+  return (
+    <div>Wrapper</div>
+  )
+}
+
+export default Wrapper
